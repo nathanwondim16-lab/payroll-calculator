c5069cbbcd345fdae8098573b1766959dae87cb3
Nathan Wondim <nathanwondim16@gmail.com>
2026-04-21 13:56:01 -0700

    Finished part 1

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class Employee {
+    private int employeeID;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    Employee() {
+
+    }
+
+    Employee(int employeeID, String name, double hoursWorked, double payRate) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
 }
--- src/main/java/Payroll.java
+++ src/main/java/Payroll.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Payroll {
+    static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("employees.csv"));
+            String line = reader.readLine(); // Skips first line
+            while((line = reader.readLine()) != null) {
+                String[] employeeInfo = line.split("\\|");
+                int employeeID = Integer.parseInt(employeeInfo[0]);
+                String name = employeeInfo[1];
+                double hoursWorked = Double.parseDouble(employeeInfo[2]);
+                double pay = Double.parseDouble(employeeInfo[3]);
+                Employee employee = new Employee(employeeID, name, hoursWorked, pay);
+                System.out.printf("ID:%-10d Name: %-20s Gross Pay:$%-20.2f\n",employee.getEmployeeID(), employee.getName(), employee.getGrossPay());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Somethiing went wrong with writing to the file");
+        }
+    }
 }
